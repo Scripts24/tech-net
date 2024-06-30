b2f1aed19c477fbd4d963f40118c94200dc7132e
Patricia Atanasoff <system.informatica365@gmail.com>
2024-06-30 01:13:52 -0300

    scrollReveal

---
+++ js/myReveal.js
@@ -0,0 +1,15 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 3000,
+    delay: 600,
+    reset: true
+})
+
+
+sr.reveal(`.section-title`, { interval: 100 })
+sr.reveal(`.title-brand, .nav-item `, { distance: '0px', opacity: 0.3, interval: 100, reset: false })
+sr.reveal(`.img-about`, { origin: 'left' })
+sr.reveal(`.text-about `, { origin: 'right' })
+sr.reveal(`.form-contact `, { origin: 'bottom' })
+sr.reveal('.service', { rotate: { x: 0, y: 180, z: 0 }, interval: 600, delay: 1000 });
