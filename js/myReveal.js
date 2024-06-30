748f2531fe51a2b1720a5cab8e46a83c4f8d08d7
Patricia Atanasoff <system.informatica365@gmail.com>
2024-06-30 05:00:49 -0300

    dark mode

--- js/myReveal.js
+++ js/myReveal.js
@@ -1,14 +1,14 @@
 const sr = ScrollReveal({
     origin: 'top',
     distance: '60px',
-    duration: 3000,
+    duration: 2500,
     delay: 600,
     reset: true
 })
 
 
 sr.reveal(`.section-title`, { interval: 100 })
-sr.reveal(`.title-brand, .nav-item `, { distance: '0px', opacity: 0.3, interval: 100, reset: false })
+sr.reveal(`.title-brand `, { distance: '0px', opacity: 0.3, interval: 100, reset: false })
 sr.reveal(`.img-about`, { origin: 'left' })
 sr.reveal(`.text-about `, { origin: 'right' })
 sr.reveal(`.form-contact `, { origin: 'bottom' })
